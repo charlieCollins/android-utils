bf74eee033e513f6b3e1a9588e3a75218cae1e89
Charlie Collins <charlie.collins@gmail.com>
2012-12-20 12:42:00 -0500

    Add connection present methods

--- src/main/java/com/totsp/android/util/NetworkUtil.java
+++ src/main/java/com/totsp/android/util/NetworkUtil.java
@@ -11,6 +11,29 @@ import android.util.Log;
 
 public class NetworkUtil {
 
+  public static boolean connectionPresent(ConnectivityManager mgr) {
+      if (mgr == null) {
+         throw new UnsupportedOperationException("Error, ConnectivityManager cannot be null");
+      }
+      NetworkInfo netInfo = mgr.getActiveNetworkInfo();
+      if ((netInfo != null) && (netInfo.getState() != null)) {
+         return netInfo.getState().equals(State.CONNECTED);
+      }
+      return false;
+   }
+
+   public static boolean wifiConnectionPresent(ConnectivityManager mgr) {
+      if (mgr == null) {
+         throw new UnsupportedOperationException("Error, ConnectivityManager cannot be null");
+      }
+      NetworkInfo netInfo = mgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+      if ((netInfo != null) && (netInfo.getState() != null)) {
+         return netInfo.getState().equals(State.CONNECTED);
+      }
+      return false;
+   }
+
+
    // also see UrlImageHelper and such projects
    public Bitmap retrieveBitmap(String urlString) {
       Log.d(Constants.LOG_TAG, "making HTTP trip for image:" + urlString);
